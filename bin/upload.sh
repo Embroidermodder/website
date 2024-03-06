a69464e676cef8346d275c3c0785ecd0e1736fc8
Robin Swift <87496244+robin-swift@users.noreply.github.com>
2024-03-06 17:37:15 +0000

    Delete bin/upload.sh

--- bin/upload.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# These commands are where we self-modify the repository and
-# they aren't allowed in the Embroidermodder or libembroidery repositories.
-mkdir downloads
-mv build/embroider downloads
-git add downloads/embroider
-git commit -m "Updating embroider daily"
-git push origin main
