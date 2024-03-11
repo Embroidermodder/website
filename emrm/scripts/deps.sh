82c084f62706640aa0aa863e0d2568e476713190
Robin Swift <robinswiftart@gmail.com>
2024-03-11 13:56:05 +0000

    moving emrm out

--- emrm/scripts/deps.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Installing required dependencies..."
-
-sudo apt-get -y update &> deps.log || exit 1
-sudo apt-get -y upgrade &>> deps.log || exit 1
-sudo apt-get -y install texlive texlive-latex-extra &>> deps.log || exit 1
-
-echo "Dependencies installed."
